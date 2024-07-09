7098ec43b74cd7761239e81b48446628ce3b52ba
ScorTnT <flalxlemdkdlel@naver.com>
2024-07-09 18:00:31 +0900

    first commit

---
+++ reactSocketIO/server.js
@@ -0,0 +1,31 @@
+import { Server } from "socket.io";
+import express from "express";
+import * as http from "http";
+import ViteExpress from "vite-express";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on('connection',(client) => {
+    console.log(client.handshake.query.userName);
+    console.log(`connection success ${client.handshake.query.userName}`);
+    client.on('disconnect', ()=>{
+        console.log(`disconnect ${client.handshake.query.userName}`);
+    });
+});
+
+server.listen(3000, ()=>{
+    console.log('Listen on 3000 port'); 
+});
+
+// routing page
+app.get("/api", (_, res)=>{
+    res.send("api page.");
+});
+
+ViteExpress.bind(app, server);
---
+++ reactSocketIO/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { io } from "socket.io-client";
+function App() {
+  const [socket, setSocket] = useState();
+  const [userName, setUserName] = useState('');
+  function connectToServer(e){
+    console.log(`connectToServer`);
+    const _socket = io('http://localhost:3000',{
+      autoConnect: false,
+      query: {
+        userName: userName,
+      }
+    });
+    _socket.connect();
+    setSocket(_socket);
+  }
+  function disConnectToServer(){
+    console.log(`disConnectToServer`);
+    socket?.disconnect();
+  }
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <h1>User :{userName}</h1>
+      <div className="card">
+        <input value={userName} onChange={e => setUserName(e.target.value)}/>
+        <button onClick={() => connectToServer()}>
+          접속
+        </button>
+        <button onClick={() => disConnectToServer()}>
+          접속 종료
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
